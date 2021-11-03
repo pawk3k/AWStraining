3932f03c640c9dc852ab562d1e6254b279b8eeb8
Pavlo <ravliv.pavlobohdan@gmail.com>
2021-11-03 22:10:52 +0100

    feat: add whole todo spec

--- todo-app/lambda/todoHandler.ts
+++ todo-app/lambda/todoHandler.ts
@@ -1,5 +1,6 @@
 /// <reference types="aws-sdk" />
 import AWS = require("aws-sdk");
+import {v4 as uuid} from "uuid"
 
 const tableName = process.env.TABLE_NAME || "";
 const dynamo = new AWS.DynamoDB.DocumentClient();
@@ -10,6 +11,10 @@ const createResponse = (
 ) => {
     return {
         statusCode,
+        headers :{
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS, POST,GET, DELETE",
+        },
         body: JSON.stringify(body, null, 2)
     };
 };
@@ -23,20 +28,50 @@ const getAllTodos = async () => {
 
     return scanResult;
 };
+const  addTodoItem = async (data:{todo: string , id : string }) =>{
+    const {id, todo} = data;
+    if(todo && todo !== ""){
+        await dynamo.put({TableName : tableName, Item :{ id : id || uuid(), todo}}).promise()
+    }
+    return todo
+}
+const deleteTodoItem = async (data:{id:string}) =>{
+    const {id} = data
+
+    if (id && id !== ""){
+        await dynamo.delete({TableName : tableName, Key: {
+            id
+        }}).promise()
+    }
+    return id
+}
 
 exports.handler = async function (event: AWSLambda.APIGatewayEvent) {
     try {
         const { httpMethod, body: requestBody } = event;
-
+        if(httpMethod === "OPTIONS"){
+            return createResponse
+        }
         if (httpMethod === "GET") {
             const response = await getAllTodos();
-
             return createResponse(response.Items || []);
         }
-        return createResponse(
-            `We only accept GET requests for now, not ${httpMethod}`,
-            500
-        );
+        if(!requestBody){
+            return createResponse('Missing request body', 500)
+        }
+        const data = JSON.parse(requestBody);
+    
+        if(httpMethod ==="POST"){
+            const todo = await addTodoItem(data);
+            return todo ? createResponse(`${todo} added to the database`) : createResponse('todo is missing', 500)
+        }
+        if(httpMethod === "DELETE"){
+            const id = await deleteTodoItem(data)
+            
+            return id ? createResponse(`${id} deleted from the database`) : createResponse('todo is missing', 500)
+        }
+        return null
+
     } catch (error) {
         console.log(error);
         return createResponse(String(error) , 500);
